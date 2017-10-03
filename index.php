59d7f7a4b06820d3347bec39575c90f6030c558d
Dallas Dituri <dcdituri@hotmail.com>
2017-10-03 17:14:20 +0000

    issa start

--- index.php
+++ index.php
@@ -1,9 +1,19 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <h1>ePortfolio</h1>
+        <h1>Dallas Dituri ePortfolio</h1>
     </head>
+    <nav>
+        <a href="index.php">Home</a>
+        <a href="experience.php">Experience</a>
+        <a href="odphi.php">Omega Delta Phi</a>
+        <a href="vision.php">Vision</a>
+        <a href="contact.php">Contact Me</a>
+
+    </nav>
     <body>
-        <p>?An ePortfolio about nothing.</p>
+    <img src="img/portrait.jpg" height=400px/>
+    <p id="me">I am a third-year at CSUMB working towards a B.S. Computer Science with a concentration in Software Engineering. I have experience in the industry working at Google as an Engineering Practicum Intern, and leadership experience being Vice President of Omega Delta Phi's Alpha Kappa Chapter.</p>
+
     </body>
 </html>
