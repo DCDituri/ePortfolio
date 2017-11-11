2ba7e0d38a0866f59fa28591b747338308e822e0
Dallas Dituri <dcdituri@hotmail.com>
2017-11-11 07:30:09 +0000

    updates

--- index.php
+++ index.php
@@ -1,19 +1,30 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <h1>Dallas Dituri ePortfolio</h1>
+        <style>@import "style.css";</style>
+        <link href="https://fonts.googleapis.com/css?family=Cinzel" rel="stylesheet">
+        <title>Dituri: A History</title>
+        <div class = "jumbotron"><header><h1>Dallas Dituri ePortfolio</h1></header></div>
     </head>
     <nav>
         <a href="index.php">Home</a>
-        <a href="experience.php">Experience</a>
-        <a href="odphi.php">Omega Delta Phi</a>
-        <a href="vision.php">Vision</a>
-        <a href="contact.php">Contact Me</a>
+        <a href="experience.html">Experience</a>
+        <a href="odp.html">Omega Delta Phi</a>
+        <a href="vision.html">Vision</a>
+        <a href="contact.html">Contact Me</a>
 
     </nav>
     <body>
-    <img src="img/portrait.jpg" height=400px/>
-    <p id="me">I am a third-year at CSUMB working towards a B.S. Computer Science with a concentration in Software Engineering. I have experience in the industry working at Google as an Engineering Practicum Intern, and leadership experience being Vice President of Omega Delta Phi's Alpha Kappa Chapter.</p>
-
+        <div class="content">
+        <div class="left">
+            <img id="port" src="img/portrait.jpg" height=400px/>
+        </div> <!-- left -->
+        <div class = "right">
+        
+            <h2>About Me</h2>
+            <p>I am a third-year at CSUMB working towards a B.S. Computer Science with a concentration in Software Engineering. I have experience in the industry working at Google as an Engineering Practicum Intern, and leadership experience being Vice President of Omega Delta Phi's Alpha Kappa Chapter.</p>
+        
+        </div> <!-- Right -->
+        </div> <!-- Content -->
     </body>
 </html>
